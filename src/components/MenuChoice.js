dfd94c511281e76f12db2f2a100990d5a27c42b3
Carlos Hernandez <carloshdzrco@gmail.com>
2022-11-04 17:30:50 -0500

    added dropdown

--- src/components/MenuChoice.js
+++ src/components/MenuChoice.js
@@ -1,7 +1,46 @@
 import React, { useState } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Dropdown, Flag } from 'semantic-ui-react'
 import '../css/MenuChoice.css'
 import {Link} from 'react-router-dom'
+
+const friendOptions = [
+  {
+    key: 'Premier League',
+    text: 'Premier League',
+    value: 'Premier League',
+    image: <Flag name='england' /> ,
+  },
+  {
+    key: 'La Liga',
+    text: 'La Liga',
+    value: 'La Liga',
+    image: <Flag name='spain' />,
+  },
+  {
+    key: 'Bundesliga',
+    text: 'Bundesliga',
+    value: 'Bundesliga',
+    image: <Flag name='germany' />,
+  },
+  {
+    key: 'Serie A',
+    text: 'Serie A',
+    value: 'Serie A',
+    image: <Flag name='italy' />,
+  },
+  {
+    key: 'Ligue 1',
+    text: 'Ligue 1',
+    value: 'Ligue 1',
+    image: <Flag name='france' />,
+  },
+  {
+    key: 'Eredivise',
+    text: 'Eredivise',
+    value: 'Eredivise',
+    image: <Flag name='netherlands' />,
+  },
+]
 
 function MenuChoice({children}) {
 
@@ -13,35 +52,43 @@ function MenuChoice({children}) {
     }
   return (
     <>
-        <Menu className='MenuChoice'>
-        {/* <li> <Link to="/">Home</Link> </li> */}
+      <Dropdown className='DropDownChoice'
+        placeholder='Select Friend'
+        fluid
+        selection
+        options={friendOptions}
+        defaultValue='Premier League'
+      />
 
-            <Menu.Item as={Link} to='/'
-            name='Standings'
-            active={activeItem === 'Standings'}
-            onClick={handleItemClick}
-            >
-            <h4 className='menuText'>Standings</h4>
-            </Menu.Item>
-            
-            <Menu.Item as={Link} to='/stats'
-            name='Statistics'
-            active={activeItem === 'Stats'}
-            onClick={handleItemClick}
-            >
-            <h4 className='menuText'>Stats</h4>
-            </Menu.Item>
+      <Menu className='MenuChoice'>
+      {/* <li> <Link to="/">Home</Link> </li> */}
 
-            <Menu.Item as={Link} to='/matches'
-            name='Matches'
-            active={activeItem === 'Matches'}
-            onClick={handleItemClick}
-            >
-            <h4 className='menuText'>Matches</h4>
-            </Menu.Item>
-        </Menu>
+          <Menu.Item as={Link} to='/'
+          name='Standings'
+          active={activeItem === 'Standings'}
+          onClick={handleItemClick}
+          >
+          <h4 className='menuText'>Standings</h4>
+          </Menu.Item>
+          
+          <Menu.Item as={Link} to='/stats'
+          name='Statistics'
+          active={activeItem === 'Stats'}
+          onClick={handleItemClick}
+          >
+          <h4 className='menuText'>Stats</h4>
+          </Menu.Item>
 
-        {children}
+          <Menu.Item as={Link} to='/matches'
+          name='Matches'
+          active={activeItem === 'Matches'}
+          onClick={handleItemClick}
+          >
+          <h4 className='menuText'>Matches</h4>
+          </Menu.Item>
+      </Menu>
+
+      {children}
     </>
   )
 }
