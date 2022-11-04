60fbfa0e2df238b08ca183c23c575e270bc23ac6
Carlos Hernandez <carloshdzrco@gmail.com>
2022-11-04 17:49:12 -0500

    added ranking label to statistics

--- src/components/StatisticsItem.js
+++ src/components/StatisticsItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Grid, Image, Segment } from 'semantic-ui-react'
+import { Grid, Image, Segment, Label } from 'semantic-ui-react'
 import '../css/StatisticsItem.css'
 
 function StatisticsItem({index}) {
@@ -8,13 +8,18 @@ function StatisticsItem({index}) {
     // const player = useSelector((state) => state.statistics.playerList[index].player)
     // console.log(player)
 
+    
+
     if(index === 0){
         return (
             <>
                 <Grid.Column className='statsColumn'>
                     {/* <Image src={player.photo} /> */}
                     
-                    <Segment><Image className='playerPicTop' src='https://react.semantic-ui.com/images/wireframe/image.png' /></Segment>
+                    <Segment>
+                    <Label attached='top left'>{index+1}</Label>
+                        <Image className='playerPicTop' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+                    </Segment>
                 </Grid.Column>
             </>
           )
@@ -25,7 +30,10 @@ function StatisticsItem({index}) {
             <>
                 <Grid.Column className='statsColumn'>
                     {/* <Image src={player.photo} /> */}
-                    <Segment><Image className='playerPicSecond' src='https://react.semantic-ui.com/images/wireframe/image.png' /></Segment>
+                    <Segment>
+                        <Label attached='top left'>{index+1}</Label>
+                        <Image className='playerPicSecond' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+                    </Segment>
 
                 </Grid.Column>
             </>
@@ -37,7 +45,10 @@ function StatisticsItem({index}) {
             <>
                 <Grid.Column className='statsColumn'>
                     {/* <Image src={player.photo} /> */}
-                    <Segment><Image className='playerPicRest' src='https://react.semantic-ui.com/images/wireframe/image.png' /></Segment>
+                    <Segment>
+                        <Label attached='top left'>{index+1}</Label>
+                        <Image className='playerPicRest' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+                    </Segment>
 
                 </Grid.Column>
             </>
